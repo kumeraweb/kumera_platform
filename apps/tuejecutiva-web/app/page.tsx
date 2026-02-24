67e5eb1a5b22483b2e2d29b179c731f641bd9403
javiernfigueroa <javiernfigueroa@gmail.com>
2026-02-24 19:56:51 -0300

    dfe

--- apps/tuejecutiva-web/app/page.tsx
+++ apps/tuejecutiva-web/app/page.tsx
@@ -3,6 +3,8 @@ import CategoryPills from "./components/CategoryPills";
 import HowItWorks from "./components/HowItWorks";
 import VerificationBlock from "./components/VerificationBlock";
 
+export const dynamic = "force-dynamic";
+
 export default function HomePage() {
   return (
     <>
