ad030bb1c28cbe84f5d4e1794044d9f810861dea
javiernfigueroa <javiernfigueroa@gmail.com>
2026-03-03 19:33:00 -0300

    Crear plan campaña Google Ads

--- apps/tractiva-web/src/global.d.ts
+++ apps/tractiva-web/src/global.d.ts
@@ -4,7 +4,6 @@ declare global {
   interface Window {
     gtag_report_conversion?: (url?: string) => boolean;
     gtag_report_wa_conversion?: (url?: string) => boolean;
-    gtag_report_phone_conversion?: (url?: string) => boolean;
     gtag_report_form_conversion?: () => boolean;
   }
 }
